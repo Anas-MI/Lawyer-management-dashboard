7bf9c497e83aa0973fce3246f2e3caac83324403
Gourav Goyal <gouravgoyal290@gmail.com>
2020-06-08 12:15:05 +0530

    css fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import '/css/.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import store from './store'
@@ -7,8 +10,7 @@ import App from './App/index';
 import * as serviceWorker from './serviceWorker';
 import config from './config';
 
-// import '/css/.min.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 
 const app = (
