aaada2281af69f51497cfd366f6a3dfc6edabc6d
Gourav Goyal <gouravgoyal290@gmail.com>
2020-06-17 15:12:20 +0530

    Cm out

--- src/App/layout/LawyerLayout/NavBar/NavRight/index.js
+++ src/App/layout/LawyerLayout/NavBar/NavRight/index.js
@@ -82,7 +82,7 @@ class NavRight extends Component {
                     {/* <li className={this.props.rtlLayout ? 'm-r-15' : 'm-l-15'}>
                         <a href={DEMO.BLANK_LINK} className="displayChatbox" onClick={() => {this.setState({listOpen: true});}}><i className="icon feather icon-mail"/></a>
                     </li> */}
-                    <li>
+                    {/* <li>
                         <Dropdown alignRight={!this.props.rtlLayout} className="drp-user">
                             <Dropdown.Toggle variant={'link'} id="dropdown-basic">
                                 <i className="icon feather icon-settings"/>
@@ -90,20 +90,20 @@ class NavRight extends Component {
                             <Dropdown.Menu alignRight className="profile-notification">
                                 <div className="pro-head">
                                     <img src={Avatar1} className="img-radius" alt="User Profile"/>
-                                    {/* <span>John Doe</span> */}
+                                    <span>John Doe</span>
                                     <Link to='/logout' className="dud-logout" title="Logout">
                                         <i className="feather icon-log-out"/>
                                     </Link>
                                 </div>
                                 <ul className="pro-body">
-                                    {/* <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-settings"/> Settings</a></li> */}
+                                    <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-settings"/> Settings</a></li>
                                     <li><a href="/profile" className="dropdown-item"><i className="feather icon-user"/> Profile</a></li>
-                                    {/* <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-mail"/> My Messages</a></li> */}
-                                    {/* <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-lock"/> Lock Screen</a></li> */}
+                                    <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-mail"/> My Messages</a></li>
+                                    <li><a href={DEMO.BLANK_LINK} className="dropdown-item"><i className="feather icon-lock"/> Lock Screen</a></li>
                                 </ul>
                             </Dropdown.Menu>
                         </Dropdown>
-                    </li>
+                    </li> */}
                 </ul>
                 <ChatList listOpen={this.state.listOpen} closed={() => {this.setState({listOpen: false});}} />
             </Aux>
