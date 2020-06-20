e16d30cc2d714cf44b774946a4a8bf704dbf289e
Jayesh Patidar <44489457+Jess70@users.noreply.github.com>
2020-06-20 18:48:15 +0530

    Update v2.jsx
    Added Fields for setting event

--- src/App/components/EventEditor/v2.jsx
+++ src/App/components/EventEditor/v2.jsx
@@ -2,9 +2,30 @@ import React, { useEffect } from "react";
 import { DateTimePickerComponent } from "@syncfusion/ej2-react-calendars";
 import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
 import { RecurrenceEditorComponent } from "@syncfusion/ej2-react-schedule";
+import { notification } from "antd";
 
 const EditorTemplate = props => {
+  function onChange(args){
+    console.log(args.itemData.value)
+    {/*
+    if(args.itemData.value==='via Email'){
 
+    }else 
+    if(args.itemData.value==='via Notification'){
+      const args = {
+        message: 'Appointment',
+        description:
+          'You have A appointment Ahead',
+        duration: 0,
+      };
+      notification.open(args)
+    }else  
+    if(args.itemData.value==='Before 10 mins'){
+
+    } */}
+    
+  }
+  
   return props !== undefined ? (
     <table
       className="custom-event-editor"
@@ -96,6 +117,38 @@ const EditorTemplate = props => {
             ></textarea>
           </td>
         </tr>
+        <tr>
+          <td className="e-textlabel">Remind Before </td>
+          <td colSpan={4}>
+            <textarea
+              id="Alert"
+              className="e-field e-input"
+              name="alert"
+              rows={2}
+              cols={50}
+              style={{
+                width: "100%",
+                height: "60px !important",
+                resize: "vertical",
+              }}
+            ></textarea>
+          </td>
+        </tr>
+        <tr>    
+                  <td className="e-textlabel">Show Reminder</td>
+                  <td colSpan={4}>
+                    <DropDownListComponent
+                      id="ReminderState"
+                      placeholder="Choose status"
+                      data-name="ReminderState"
+                      className="e-field"
+                      style={{ width: "100%" }}
+                      change={(e)=>onChange(e)}
+                      dataSource={["via Email", "via Notification", "Before 10 mins"]}
+                      value={props.EventType || null}
+                    ></DropDownListComponent>
+                  </td>
+                </tr>
       </tbody>
     </table>
   ) : (
