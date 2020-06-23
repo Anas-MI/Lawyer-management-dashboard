39ccd12266b5c90e3024518b4f0b44be46849d63
Jayesh Patidar <44489457+Jess70@users.noreply.github.com>
2020-06-23 20:16:07 +0530

    Create index.js

---
+++ src/App/components/AddEditContact/DynamicFeilds/index.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import { Form,Button, Row , Col } from "react-bootstrap";
+
+
+
+class DynamicFeilds extends React.Component {
+    render(){
+    console.log("Dynamic Called")
+    return (
+        <div >
+            {
+            this.props.inputList.map((val, idx)=> {
+                  let inputId = `input-${idx}`
+               
+                  return (
+                    <div key={idx}>
+                      <Form.Group controlId={this.props.type}>
+                        <Form.Label>{this.props.type}</Form.Label>
+                         <Form.Control name={this.props.type} className={this.props.type} type="text" placeholder={this.props.type} 
+                         onChange={(e,idx)=>this.props.change(e,idx)}/>
+                      </Form.Group>
+                    </div>
+                  )
+                })}
+        </div>
+    )
+}}
+
+export default DynamicFeilds
