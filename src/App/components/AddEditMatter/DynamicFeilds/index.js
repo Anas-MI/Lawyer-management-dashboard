15f61e7306201bb76007158d3c75a379bc04aefd
Jayesh Patidar <44489457+Jess70@users.noreply.github.com>
2020-06-30 21:20:37 +0530

    Update index.js
    API Integration

--- src/App/components/AddEditMatter/DynamicFeilds/index.js
+++ src/App/components/AddEditMatter/DynamicFeilds/index.js
@@ -1,41 +1,57 @@
 import React, { useState, useEffect } from 'react'
 import { Form,Button, Row , Col } from "react-bootstrap";
+import api from '../../../../resources/api'
 
-
-
+let response = {}
+let optns = null
 class DynamicFeilds extends React.Component {
   constructor(props){
     super(props)
+    this.state = {
+      InputList : this.props.InputList
+    }
   }
-  
 
+  async componentWillMount(){
+   // if(prevProps.InputList !== this.props.InputList) {
+   //   this.setState({InputList: this.props.InputList});
+   // }
+   await api.get('/contact/showall').then(res=>{
+    response = res.data}
+    )
+
+  }
+   componentWillUpdate(){
+    optns = response.data.map((value, index)=>{
+      return <option>{value.firstName}</option>
+    })
+    
+   }
+ 
     render(){
-      
+       
+
+
     return (
         <div >
             {
-            this.props.InputList.map((val, idx)=> {
+            this.state.InputList.map((val, idx)=> {
                   let inputId = `input-${idx}`
                
                   return (
                     <div key={idx}>
                     <Row>
                       <Col>
-                            <Form.Group controlId="formGroupRelationship">
+                            <Form.Group controlId={idx}>
                                 <Form.Label>Relationship</Form.Label>
-                                <Form.Control name='Relationship' type="text" placeholder="Relationship" 
-                                value={val['Relationship']} onChange={this.props.change}/>
+                                <Form.Control name='Relationship' type="text" placeholder="Relationship"  onChange={this.props.change}/>
                             </Form.Group>
                      </Col>
                      <Col>
-                          <Form.Group controlId="exampleForm.ControlSelect1">
+                          <Form.Group controlId={idx}>
                           <Form.Label>Contact</Form.Label>
                           <Form.Control as="select">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
+                            {optns}
                          </Form.Control>
                          </Form.Group>
                     </Col>
