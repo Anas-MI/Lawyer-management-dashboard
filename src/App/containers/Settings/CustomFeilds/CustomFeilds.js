0bb9966bb4d20de44d6e607372b3f529aff04c3b
Jayesh Patidar <44489457+Jess70@users.noreply.github.com>
2020-07-01 16:11:16 +0530

    Update CustomFeilds.js
    API integration

--- src/App/containers/Settings/CustomFeilds/CustomFeilds.js
+++ src/App/containers/Settings/CustomFeilds/CustomFeilds.js
@@ -3,10 +3,17 @@ import { Tabs, Button,Modal , Card } from 'antd';
 import Matter from './Matter/matter'
 import Contact from './Contact/contact'
 import { Form } from 'react-bootstrap'
+import api from '../../../../resources/api'
 const { TabPane } = Tabs;
 
 
 class customFeilds extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+
+    }
+  }
   state = {
     modal1Visible: false,
     modal2Visible: false,
@@ -19,8 +26,17 @@ class customFeilds extends React.Component {
   setModal2Visible(modal2Visible) {
     this.setState({ modal2Visible });
   }
+  
 
   render() {
+    const HandleChange=(e)=>{
+      e.persist()
+      this.setState(st=>({...st,[e.target.name]:e.target.value}))
+    }
+    const HandleOk=()=>{
+    api.post('http://localhost:3002/api/user/update/5eecb08eaec6f1001765f8d5',this.state)
+      this.setModal2Visible(false)
+    }
     const operations = <Button onClick={() => this.setModal2Visible(true)}>Add</Button>
     return (
       <Card>
@@ -37,17 +53,17 @@ class customFeilds extends React.Component {
           title="Add Custom Feild"
           centered
           visible={this.state.modal2Visible}
-          onOk={() => this.setModal2Visible(false)}
+          onOk={HandleOk}
           onCancel={() => this.setModal2Visible(false)}
         >
           <Form>
             <Form.Group controlId="Name">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="Name" placeholder="Name" />
+                <Form.Control type="Name" placeholder="Name" name="name" onChange={HandleChange}/>
             </Form.Group>
             <Form.Group controlId="Type">
                 <Form.Label>Select Custom Feild type</Form.Label>
-                <Form.Control as="select">
+                <Form.Control as="select" name="type" onChange={HandleChange}>
                 <option>Checkbox</option>
                 <option>Contact Select</option>
                 <option>Date</option>
@@ -63,11 +79,15 @@ class customFeilds extends React.Component {
                     type="checkbox"
                     id={`Default`}
                     label={`Default`}
+                    name="default"
+                    onChange={HandleChange}
                 /><br></br>
                 <Form.Check 
                     type="checkbox"
                     id={`Required`}
                     label={`Required`}
+                    name="required"
+                    onChange={HandleChange}
                 />
 
             </Form.Group>
