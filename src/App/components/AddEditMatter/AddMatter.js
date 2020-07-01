7904c32f635c31581bf10be3fab0acf02022059f
Jayesh Patidar <44489457+Jess70@users.noreply.github.com>
2020-07-01 16:13:51 +0530

    Update AddMatter.js
    API integration

--- src/App/components/AddEditMatter/AddMatter.js
+++ src/App/components/AddEditMatter/AddMatter.js
@@ -1,47 +1,73 @@
 import React, { useState, useEffect, useDispatch, useSelector} from 'react'
-import { Form, Row , Col, Button} from "react-bootstrap";
+import { Form, Row , Col , Button } from "react-bootstrap";
 import { message,  Modal, Card } from 'antd';
 import api from '../../../resources/api'
 import AddPerson from '../AddEditContact/AddPerson'
 import DynamicFeild from '../AddEditMatter/DynamicFeilds/index'
-const AddEditMatter = props => {
 
-    const [state,setState] = useState({Relation : [{Relationship : "" , Contact : ""}]})
-    const [InputList, setInputList] = useState([{Relationship : "" , Contact : ""}])
-    const [modal , setModal] = useState()
-    const [editMode,setEditMode] = useState(false)
-  const addFeild=() =>{
-    let list = InputList
-    list.push({Relationship : "" , Contact : ""})
-    setInputList(list)
-    let newState= state
-    newState.Relation = InputList
-    setState(newState)
-    console.log(state)
+let res = {}
+let customFields = null
+let contacts = {}
+let optns = null
+class AddEditMatter extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      InputList  : [{Relationship : "" , Contact : ""}],
+      modal : false,
+      editMode : false
+    }
+
   }
+  async componentDidMount(){
+    res = await api.get('/user/view/5eecb08eaec6f1001765f8d5').then(
+    contacts = await api.get('/contact/showall'))
+  }
+  componentWillUpdate(){
+    console.log(contacts)
+    optns = contacts.data.data.map((value, index)=>{
+      return <option key={index}>{value.firstName}</option>
+     })
+    customFields = res.data.data.customFields.map((value, index)=>{
+    
+      return <Form.Group key={index} controlId={index}>
+              <Form.Label>{value.name}</Form.Label>
+              <Form.Control name={value.name} type={value.type} placeholder={value.name} />
+              </Form.Group>
+    })
+  }
+  render(){
+    
+    const addFeild=() =>{
+      let list = this.state.InputList
+      list.push({Relationship : "" , Contact : ""})
+      this.setState({InputList : list})
+      let newState= this.state
+      newState.Relation = this.state.InputList
+      this.setState(newState)
+    }
   
   const handleChange = (e) => {
     e.persist()
-    setState(st=>({...st,[e.target.name]:e.target.value}))
+    this.setState(st=>({...st,[e.target.name]:e.target.value}))
   }
   const HandleDynamicChange = (e)=>{
     e.persist()
-    let list = state
+    let list = this.state
     const { id , value } = e.target
     list.Relation[id].Relationship = value
-    setState(list)
-    console.log(state)
+    this.setState(list)
   }
 
  
     const handleSubmit = e => {
         e.preventDefault()
-        if(editMode){
+        if(this.state.editMode){
            //  dispatch(updateBlog({id:state._id,body:state}))
         }else{
-           api.post('matter/create', state)
+           api.post('matter/create', this.state)
         }
-        props.history.goBack()
+        this.props.history.goBack()
     }
   
     
@@ -52,28 +78,26 @@ const AddEditMatter = props => {
         </div>
         <Card title="Matter Information" className="mb-4">
           <Form className="form-details">
+            
             <Form.Group controlId="exampleForm.ControlSelect1">
               <Form.Label>Client</Form.Label>
                 <Form.Control as="select">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
+                  
+                  {optns}
                 </Form.Control>
               </Form.Group>
               <div className="form-add mb-4">
-                <span onClick={() => setModal(true)}>Add Contact</span>
+                <span onClick={() => this.setState({modal:true})}>Add Contact</span>
               </div>
               <Form.Group controlId="formGroupMatter">
                 <Form.Label>Matter Description</Form.Label>
                   <Form.Control name='Matter' as="textarea" rows="3" type="text" placeholder="Matter description" 
-                    value={state['Matter']} onChange={handleChange}/>
+                    value={this.state['Matter']} onChange={handleChange}/>
                 </Form.Group>
                 <Form.Group controlId="formGroupClientRefenceNumber">
                   <Form.Label>Client reference number</Form.Label>
                   <Form.Control name='ClientRefenceNumber' type="text" placeholder="Client Refence Number" 
-                    value={state['ClientRefenceNumber']} onChange={handleChange}/>
+                    value={this.state['ClientRefenceNumber']} onChange={handleChange}/>
                 </Form.Group>
                 <Form.Group controlId="exampleForm.ControlSelect1">
                   <Form.Label>Practise Area</Form.Label>
@@ -98,21 +122,21 @@ const AddEditMatter = props => {
                     <Form.Group controlId="formGroupOpenDate">
                     <Form.Label>Open Date</Form.Label>
                     <Form.Control name='OpenDate' type="Date" placeholder="OpenDate" 
-                      value={state['OpenDate']} onChange={handleChange}/>
+                      value={this.state['OpenDate']} onChange={handleChange}/>
                     </Form.Group>
                   </Col>
                   <Col>  
                     <Form.Group controlId="formGroupClosing Date">
                     <Form.Label>Closing Date</Form.Label>
                     <Form.Control name='Closing Date' type="Date" placeholder="Closing Date" 
-                      value={state['Closing Date']} onChange={handleChange}/>
+                      value={this.state['Closing Date']} onChange={handleChange}/>
                     </Form.Group>
                   </Col>  
                   <Col>
                     <Form.Group controlId="formGroupPendingDate">
                     <Form.Label>Pending Date</Form.Label>
                     <Form.Control name='PendingDate' type="Date" placeholder="PendingDate" 
-                      value={state['PendingDate']} onChange={handleChange}/>
+                      value={this.state['PendingDate']} onChange={handleChange}/>
                     </Form.Group>
                   </Col> 
                 </Form.Row>
@@ -121,7 +145,7 @@ const AddEditMatter = props => {
 
         <Card title="Related Contacts" className="mb-4">
             <Form className="form-details">
-            <DynamicFeild InputList={InputList} change={HandleDynamicChange}></DynamicFeild>
+            <DynamicFeild InputList={this.state.InputList} option={optns} change={HandleDynamicChange}></DynamicFeild> 
 
               <Form.Group controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Bill this contact" id={`check`}/>
@@ -134,24 +158,11 @@ const AddEditMatter = props => {
         </Card>
         <Card title="Custom Feilds"  className="mb-4">
         <Form className="form-details">
-          <Form.Row>
-            <Col>
-              <Form.Group controlId="formGroupName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control name='Name' type="text" placeholder="Name" 
-                value={state['Name']} onChange={handleChange}/>
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group controlId="formGroupText">
-                <Form.Label>Text</Form.Label>
-                <Form.Control name='Text' type="text" placeholder="MText" 
-                value={state['Text']} onChange={handleChange}/>
-              </Form.Group>
-            </Col>
-          </Form.Row>
-          <Button className="btn btn-success">Save</Button>
+        <p>Customise your<Button variant="link" onClick={()=>this.props.history.push('/settings/customFeilds')}>Custom Feild</Button></p>
+
+        {customFields}
         </Form>
+          <Button className="btn btn-success">Save</Button>
         </Card>
         <Card title="Billing Preference"  className="mb-4">
           <Form className="form-details">
@@ -183,15 +194,17 @@ const AddEditMatter = props => {
         <Modal
           title="Add Company"
           centered
-          visible={modal}
-          onOk={() => setModal(false)}
-          onCancel={() => setModal(false)}
+          visible={this.state.modal}
+          onOk={() => this.setState({modal :false})}
+          onCancel={() => this.setState({modal :false})}
         >
         <AddPerson></AddPerson>
 
         </Modal>
         </div>
     )
+  }
 }
+
 
 export default AddEditMatter
