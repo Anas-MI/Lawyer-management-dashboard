689ee0233ed69c50a4d56b30c45b80380bb2b456
Jayesh Patidar <44489457+Jess70@users.noreply.github.com>
2020-07-02 23:50:43 +0530

    Update upcomingTasks.js
    API Integration

--- src/App/containers/Tasks/UpcomingTasks/upcomingTasks.js
+++ src/App/containers/Tasks/UpcomingTasks/upcomingTasks.js
@@ -7,22 +7,22 @@ function callback(key) {
   console.log(key);
 }
 
-const UpcomingTasks = () => (
+const UpcomingTasks = (props) => (
   <Tabs defaultActiveKey="1" onChange={callback}>
     <TabPane tab="All" key="1">
-     <Content></Content>
+     <Content tableData={props.tableData}></Content>
     </TabPane>
     <TabPane tab="Due this Week" key="2">
-      <Content></Content>
+      <Content tableData={props.tableData}></Content>
     </TabPane>
     <TabPane tab="Due Today" key="3">
-      <Content></Content>
+      <Content tableData={props.tableData}></Content>
     </TabPane>
     <TabPane tab="Due Tomorow" key="4">
-      <Content></Content>
+      <Content tableData={props.tableData}></Content>
     </TabPane>
     <TabPane tab="Overdue" key="5">
-      <Content></Content>
+      <Content tableData={props.tableData}></Content>
     </TabPane>
   </Tabs>
 );
