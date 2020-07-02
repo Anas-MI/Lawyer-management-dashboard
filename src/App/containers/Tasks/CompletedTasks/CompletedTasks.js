d85e69cc2f428c7de89ff1e431f3e487d4a1a583
Jayesh Patidar <44489457+Jess70@users.noreply.github.com>
2020-07-02 23:55:29 +0530

    Update CompletedTasks.js
    API Integration

--- src/App/containers/Tasks/CompletedTasks/CompletedTasks.js
+++ src/App/containers/Tasks/CompletedTasks/CompletedTasks.js
@@ -1,8 +1,24 @@
 import React from 'react'
-import Content from './Content/Content'
+//import Content from './Content/Content'
 
-function CompletedTask(){
-    return <Content></Content>
+function CompletedTask(props){
+    const tableData = props.tableData 
+    return <div>
+    <table class="table">
+      <thead class="thead-light">
+        <tr>
+          <th scope="col">Date</th>
+          <th scope="col">Discription</th>
+          <th scope="col">Task</th>
+          <th scope="col">Matter</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData}
+      </tbody>
+  </table>
+  </div>     
+    
 
 }
 export default CompletedTask
