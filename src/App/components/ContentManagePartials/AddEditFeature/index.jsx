b93405f1390ba826156d86508c6ff5f65b5a752e
Gourav Goyal <gouravgoyal290@gmail.com>
2020-07-03 20:15:14 +0530

    Validation in Feature

--- src/App/components/ContentManagePartials/AddEditFeature/index.jsx
+++ src/App/components/ContentManagePartials/AddEditFeature/index.jsx
@@ -5,8 +5,13 @@ import { Form,Button } from "react-bootstrap";
 import { notification } from "antd";
 
 const AddEditFeature = (props) => {
-  const [state, setState] = useState({});
+  const [state, setState] = useState({
+    title: "",
+    description: "",
+  });
   const [editMode, setEditMode] = useState(false);
+  const [display, setDisplay] = useState(false);
+  const [error, setError] = useState({});
 
   const dispatch = useDispatch();
   const selectedFeature = useSelector((state) => state.Feature.selected);
@@ -22,35 +27,84 @@ const AddEditFeature = (props) => {
 
   const handleChange = (e) => {
     e.persist();
+    const { name, value } = e.target;
+    let errors = error;
+    switch (name) {
+      case "title":
+        errors.title =
+            (value.length == 0) 
+            ? "Title is Required"
+            : (value.length > 20) 
+            ? "Title is too Long" :""
+       break;
+       case "description":
+        errors.description =
+            (value.length == 0) 
+            ? "Description is required"
+            : (value.length > 120) 
+            ? "Description is too long" 
+            : (value.length < 50) 
+            ? "Description is too Short" :""
+       break;
+      default:
+        break;
+    }
+    setError({ ...errors });
     setState((st) => ({ ...st, [e.target.name]: e.target.value }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (editMode) {
-      dispatch(updateFeature({id:state._id,body:state},(err,response)=>{
-        if(err){
-          notification.error(err)
-        }else{
-          notification.success(response)
-        }
-      }));
-    } else {
-      dispatch(createFeature(state,(err,response)=>{
-        if(err){
-          notification.error(err)
-        }else{
-          notification.success(response)
-        }
-      }));
+
+    if(!display){
+      const validateForm = (error) => {
+        let valid = true;
+        Object.values(error).forEach((val) => val.length > 0 && (valid = false));
+        return valid;
+      };
+      if (validateForm(error)) {
+        checkValidity();
+      } else {
+        setDisplay(true)
+        return notification.warning({
+          message: "Failed to Add New Feature",
+        });
+      }
     }
-    props.history.goBack()
   };
+
+  function checkValidity() {
+    if (!Object.keys(state).every((k) => state[k] !== "")) {
+      setDisplay(true)
+      return notification.warning({
+        message: "Fields Should Not Be Empty",
+      });
+    } else {
+      if (editMode) {
+        dispatch(updateFeature({id:state._id,body:state},(err,response)=>{
+          if(err){
+            notification.error(err)
+          }else{
+            notification.success(response)
+          }
+        }));
+      } else {
+        dispatch(createFeature(state,(err,response)=>{
+          if(err){
+            notification.error(err)
+          }else{
+            notification.success(response)
+          }
+        }));
+      }
+  }
+  props.history.goBack()
+}
 
   return (
     <div className='w-75 m-auto'>
         <h3 className='text-center' >Add New Feature</h3>
-    <Form>
+    <Form className="form-details">
         <Form.Group controlId="formGroupEmail">
           <Form.Label>Title</Form.Label>
           <Form.Control
@@ -60,6 +114,7 @@ const AddEditFeature = (props) => {
             value={state["title"]}
             onChange={handleChange}
           />
+          <p className="help-block text-danger">{error.title}</p>
         </Form.Group>
         <Form.Group controlId="formGroupEmail">
           <Form.Label>Description</Form.Label>
@@ -69,7 +124,9 @@ const AddEditFeature = (props) => {
             placeholder="Description"
             value={state["description"]}
             onChange={handleChange}
+            as="textarea" rows="2"
           />
+          <p className="help-block text-danger">{error.description}</p>
         </Form.Group>
         <Button onClick={handleSubmit}>{editMode ? "Update" : "Create"}</Button>
       </Form>
