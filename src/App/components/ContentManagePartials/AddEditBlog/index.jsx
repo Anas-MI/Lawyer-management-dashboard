5e88fb5876e04706ca7e485620520fd425aa03ed
Gourav Goyal <gouravgoyal290@gmail.com>
2020-07-03 19:53:14 +0530

    Validtion in blog

--- src/App/components/ContentManagePartials/AddEditBlog/index.jsx
+++ src/App/components/ContentManagePartials/AddEditBlog/index.jsx
@@ -4,9 +4,19 @@ import { updateBlog, createBlog } from "../../../../store/Actions";
 import { Form, Button } from "react-bootstrap";
 import { notification } from "antd";
 
+const validNameRegex = RegExp(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u);
+
+
 const AddEditBlog = (props) => {
-  const [state, setState] = useState({});
+  const [state, setState] = useState({
+    title: "",
+    author: "",
+    shortDescription: "",
+    description:"",
+  });
   const [editMode, setEditMode] = useState(false);
+  const [display, setDisplay] = useState(false);
+  const [error, setError] = useState({});
 
   const dispatch = useDispatch();
   const selectedBlog = useSelector((state) => state.Blog.selected);
@@ -22,39 +32,102 @@ const AddEditBlog = (props) => {
 
   const handleChange = (e) => {
     e.persist();
+    setDisplay(false)
+    const { name, value } = e.target;
+    let errors = error;
+    switch (name) {
+      case "title":
+        errors.title =
+            (value.length == 0) 
+            ? "Title is Required"
+            : (value.length > 60) 
+            ? "Title is too Long" :""
+       break;
+       case "author":
+        errors.author =
+            (value.length == 0) 
+            ? "Author is required"
+            : (!validNameRegex.test(value))
+            ? "Author Name must be in characters!": ""
+       break;
+       case "shortDescription":
+        errors.shortDescription =
+            (value.length == 0) 
+            ? "Short Description is required"
+            : (value.length > 110) 
+            ? "Short Description is too Long" 
+            : (value.length < 50) 
+            ? "Short Description is too Short" :""
+       break;
+       case "description":
+        errors.description =
+            (value.length == 0) 
+            ? "Description is required"
+            : (value.length < 110) 
+            ? "Description is too Short" :""
+       break;
+      default:
+        break;
+    }
+    setError({ ...errors });
     setState((st) => ({ ...st, [e.target.name]: e.target.value }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (editMode) {
-      dispatch(
-        updateBlog({ id: state._id, body: state }, (err, response) => {
-          if (err) {
-            notification.error(err);
-          } else {
-            notification.success(response);
-          }
-        })
-      );
+    if(!display){
+      const validateForm = (error) => {
+        let valid = true;
+        Object.values(error).forEach((val) => val.length > 0 && (valid = false));
+        return valid;
+      };
+      if (validateForm(error)) {
+        checkValidity();
+      } else {
+        setDisplay(true)
+        return notification.warning({
+          message: "Failed to Add New Blog",
+        });
+      }
+    }
+  };
+
+  function checkValidity() {
+    if (!Object.keys(state).every((k) => state[k] !== "")) {
+      setDisplay(true)
+      return notification.warning({
+        message: "Fields Should Not Be Empty",
+      });
     } else {
-      dispatch(
-        createBlog(state, (err, response) => {
-          if (err) {
-            notification.error(err);
-          } else {
-            notification.success(response);
-          }
-        })
-      );
+      if (editMode) {
+        dispatch(
+          updateBlog({ id: state._id, body: state }, (err, response) => {
+            if (err) {
+              notification.error(err);
+            } else {
+              notification.success(response);
+            }
+          })
+        );
+      } else {
+        dispatch(
+          createBlog(state, (err, response) => {
+            if (err) {
+              notification.error(err);
+            } else {
+              notification.success(response);
+            }
+          })
+        );
+      }
     }
     props.history.goBack();
-  };
+  }
 
   return (
     <div className="w-75 m-auto">
       <h3 className="text-center">Add New Blog</h3>
-      <Form>
+      <Form className="form-details">
         <Form.Group controlId="formGroupEmail">
           <Form.Label>Title</Form.Label>
           <Form.Control
@@ -64,6 +137,7 @@ const AddEditBlog = (props) => {
             value={state["title"]}
             onChange={handleChange}
           />
+          <p className="help-block text-danger">{error.title}</p>
         </Form.Group>
         <Form.Group controlId="formGroupEmail">
           <Form.Label>Author</Form.Label>
@@ -74,6 +148,7 @@ const AddEditBlog = (props) => {
             value={state["author"]}
             onChange={handleChange}
           />
+          <p className="help-block text-danger">{error.author}</p>
         </Form.Group>
 
         <Form.Group controlId="formGroupEmail">
@@ -81,10 +156,11 @@ const AddEditBlog = (props) => {
           <Form.Control
             name="shortDescription"
             type="text"
-            placeholder="shortDescription"
+            placeholder="Short Description"
             value={state["shortDescription"]}
             onChange={handleChange}
           />
+          <p className="help-block text-danger">{error.shortDescription}</p>
         </Form.Group>
 
         <Form.Group controlId="formGroupEmail">
@@ -95,7 +171,9 @@ const AddEditBlog = (props) => {
             placeholder="Description"
             value={state["description"]}
             onChange={handleChange}
+            as="textarea" rows="3"
           />
+          <p className="help-block text-danger">{error.description}</p>
         </Form.Group>
         <Button onClick={handleSubmit}>{editMode ? "Update" : "Create"}</Button>
       </Form>
