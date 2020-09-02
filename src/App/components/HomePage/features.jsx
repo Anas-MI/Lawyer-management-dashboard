94a519161c3ef9c067b3e4eb8ee23bfb70f10cdb
Jayesh Patidar <jppatidarjay713@gmail.com>
2020-09-02 10:58:08 +0530

    resposive design for homepage

--- src/App/components/HomePage/features.jsx
+++ src/App/components/HomePage/features.jsx
@@ -25,21 +25,21 @@ componentDidMount () {
     return (
         <div id="features" className="features">
           <div className="container">
-            <div className="row">
+            <div className="row ">
               <div className="col-md-12 col-md-offset-1 section-title">
                 <h2 className="text-center">Features</h2>
               </div>
               <div className="col-lg-12 text-center">
                 <img className="img-fluid" src="https://legodesk.com/intro_css_js/images/feature-laptop.png" alt="img" />
               </div>
-              <div className="work_inner row d-flex justify-content-between mx-2">
+              <div className="work_inner fullwidth row d-flex justify-content-between mx-2">
                 { this.state.features.map( feature => 
-                  <div className="col-lg-4 d-flex align-items-stretch overflow-hidden" key={feature.id}>
-                    <div className="work_item w-100 align-items-center">
-                      <img className="img-fluid" src={dashboard} alt="img" />
-                      <a><h4 className="text-break">{feature.title}</h4></a>
-                      <p className="text-break">{feature.description}</p>
-                    </div>
+                  <div  className="col-lg-4 d-flex align-items-stretch overflow-hidden " key={feature.id}>
+                      <div className="work_item w-100 align-items-center">
+                        <img className="img-fluid" src={dashboard} alt="img" />
+                        <a><h4 className="text-break">{feature.title}</h4></a>
+                        <p className="text-break">{feature.description}</p>
+                      </div>
                   </div>                                              
                 )}
               </div>
