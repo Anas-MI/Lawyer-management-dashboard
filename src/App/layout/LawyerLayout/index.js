596c5298d24dc028b32897d2ada6e0052b3ffb9c
Jayesh Patidar <jppatidarjay713@gmail.com>
2020-09-10 15:29:06 +0530

    Bug fix

--- src/App/layout/LawyerLayout/index.js
+++ src/App/layout/LawyerLayout/index.js
@@ -73,124 +73,126 @@ class LawyerLayout extends Component {
             
             let data = window.localStorage.getItem('notifications')
             const user = JSON.parse(window.localStorage.getItem('Case.user'))
-            api.get('/calendar/viewforuser/'+ user.token.user._id).then(res=>{
+            if(user.token != null){
+                api.get('/calendar/viewforuser/'+ user.token.user._id).then(res=>{
            
-                res.data.data.map((value, index)=>{
-                   
-                    const StartTime = value.startTime? value.startTime : ""
-                    const Description = value.title
-                    const matter = value.matter ?  value.matter.matterDescription : ""
-                    const timeForReminder = value.timeForReminder ? value.timeForReminder : ""
-                    
-                    if(value.notification == true){
-
-                        const alertdate =  this.convertTime(value.startTime)
+                    res.data.data.map((value, index)=>{
                        
-                        let ddd = alertdate.getDate();
-                        let mmm = alertdate.getMonth()+1; 
-                        let yyyyy = alertdate.getFullYear();
-                       // let hourss = timeForReminder.getHours() > 12 ? timeForReminder.getHours() - 12 : timeForReminder.getHours()  ;
-                      //  let minss = timeForReminder.getMinutes() ;
-                        let hourss = timeForReminder.split(':')[0];
-                        let minss = timeForReminder.split(':')[1];
-                        minss = minss ?  minss.substring(0,2) : ""
-                       
-                       
-                       
-                        /*
-                            if(ddd<10) {
-                                ddd = '0' + dd
-                            }          
-                            if(mmm<10) {
-                                mmm = '0' + mm
-                            }
-        */            
-                        let today = new Date();
-                        let dd = today.getDate();
-                        let mm = today.getMonth()+1; 
-                        let yyyy = today.getFullYear();
-                        let hours = today.getHours() > 12 ? today.getHours() - 12 : today.getHours() ;
-                        let plusonehour = parseInt(hours) + 1
-                        console.log(plusonehour)
-                        let mins = today.getMinutes() < 10 ? '0' + today.getMinutes()  :  today.getMinutes()
-                        let plusmins = parseInt(mins + 0)
-                       
-                        console.log("1" + ddd + "/" + mmm + "/" + yyyyy + " " + hourss + ":" + minss)
-                        console.log("2" +dd + "/" + mm + "/" + yyyy + " " + hours + ":" + mins)
-                        /*
-                            if(dd<10) {
-                                dd = '0'+dd
-                            } 
-                            
-                            if(mm<10) {
-                                mm = '0'+mm
-                            } 
-                        console.log("1" + ddd + "/" + mmm + "/" + yyyyy + " " + hourss + ":" + minss)
-                        console.log("2" +dd + "/" + mm + "/" + yyyy + " " + hours + ":" + mins)
-                        /*
-                        if ( minss == "00" && ddd==dd && mmm==mm && yyyy==yyyyy && plusonehour  == hourss && ( plusmins == 00 )){
-                            notification.open({
-                                message: Description,
-                                description:
-                                    'Event assosiated to matter ' + matter + 'starts at ' + StartTime,
-                                className: 'custom-class',
-                                style: {
-                                  width: 600,
-                                },
-                              });
-                            console.log(value)
+                        const StartTime = value.startTime? value.startTime : ""
+                        const Description = value.title
+                        const matter = value.matter ?  value.matter.matterDescription : ""
+                        const timeForReminder = value.timeForReminder ? value.timeForReminder : ""
                         
-                        }else
-                        */
-                        if(ddd==dd && mmm==mm && yyyy==yyyyy && hours == hourss &&  minss == mins  ){
-                            alert("Event : " + Description + "\n" + 'Event assosiated to matter ' + matter + 'starts at ' + StartTime )
-                            const notificationtoshow =  {
-                                description: Description,
-                                matter : matter,
-                                startTime : StartTime
-                            }
-
-                            if(data == null){
-                                window.localStorage.setItem('notification', JSON.stringify([{ 
+                        if(value.notification == true){
+    
+                            const alertdate =  this.convertTime(value.startTime)
+                           
+                            let ddd = alertdate.getDate();
+                            let mmm = alertdate.getMonth()+1; 
+                            let yyyyy = alertdate.getFullYear();
+                           // let hourss = timeForReminder.getHours() > 12 ? timeForReminder.getHours() - 12 : timeForReminder.getHours()  ;
+                          //  let minss = timeForReminder.getMinutes() ;
+                            let hourss = timeForReminder.split(':')[0];
+                            let minss = timeForReminder.split(':')[1];
+                            minss = minss ?  minss.substring(0,2) : ""
+                           
+                           
+                           
+                            /*
+                                if(ddd<10) {
+                                    ddd = '0' + dd
+                                }          
+                                if(mmm<10) {
+                                    mmm = '0' + mm
+                                }
+            */            
+                            let today = new Date();
+                            let dd = today.getDate();
+                            let mm = today.getMonth()+1; 
+                            let yyyy = today.getFullYear();
+                            let hours = today.getHours() > 12 ? today.getHours() - 12 : today.getHours() ;
+                            let plusonehour = parseInt(hours) + 1
+                            console.log(plusonehour)
+                            let mins = today.getMinutes() < 10 ? '0' + today.getMinutes()  :  today.getMinutes()
+                            let plusmins = parseInt(mins + 0)
+                           
+                            console.log("1" + ddd + "/" + mmm + "/" + yyyyy + " " + hourss + ":" + minss)
+                            console.log("2" +dd + "/" + mm + "/" + yyyy + " " + hours + ":" + mins)
+                            /*
+                                if(dd<10) {
+                                    dd = '0'+dd
+                                } 
+                                
+                                if(mm<10) {
+                                    mm = '0'+mm
+                                } 
+                            console.log("1" + ddd + "/" + mmm + "/" + yyyyy + " " + hourss + ":" + minss)
+                            console.log("2" +dd + "/" + mm + "/" + yyyy + " " + hours + ":" + mins)
+                            /*
+                            if ( minss == "00" && ddd==dd && mmm==mm && yyyy==yyyyy && plusonehour  == hourss && ( plusmins == 00 )){
+                                notification.open({
+                                    message: Description,
+                                    description:
+                                        'Event assosiated to matter ' + matter + 'starts at ' + StartTime,
+                                    className: 'custom-class',
+                                    style: {
+                                      width: 600,
+                                    },
+                                  });
+                                console.log(value)
+                            
+                            }else
+                            */
+                            if(ddd==dd && mmm==mm && yyyy==yyyyy && hours == hourss &&  minss == mins  ){
+                                alert("Event : " + Description + "\n" + 'Event assosiated to matter ' + matter + 'starts at ' + StartTime )
+                                const notificationtoshow =  {
                                     description: Description,
                                     matter : matter,
                                     startTime : StartTime
-                                }]))
-                            }else{
-                                data.push(notificationtoshow)
-                                window.localStorage.setItem('notification', JSON.stringify(data))
+                                }
+    
+                                if(data == null){
+                                    window.localStorage.setItem('notification', JSON.stringify([{ 
+                                        description: Description,
+                                        matter : matter,
+                                        startTime : StartTime
+                                    }]))
+                                }else{
+                                    data.push(notificationtoshow)
+                                    window.localStorage.setItem('notification', JSON.stringify(data))
+                                }
+                                
+                               /*
+                                notification.open({
+                                    message: Description,
+                                    description:
+                                        'Event assosiated to matter ' + matter + 'starts at ' + StartTime,
+                                    className: 'custom-class',
+                                    style: {
+                                      width: 600,
+                                    },
+                                  });
+                                console.log(value)
+                                */
                             }
-                            
-                           /*
-                            notification.open({
-                                message: Description,
-                                description:
-                                    'Event assosiated to matter ' + matter + 'starts at ' + StartTime,
-                                className: 'custom-class',
-                                style: {
-                                  width: 600,
-                                },
-                              });
-                            console.log(value)
-                            */
+                            else{
+                                console.log("i min passed")
+                            }   
                         }
-                        else{
-                            console.log("i min passed")
-                        }   
-                    }
-                    /*
-                    const tableData={
-                        id: value._id,
-                        Subject : value.title,
-                       
-                        TimeForReminder : value.timeForReminder,
-                        Matter : value.matter ?  value.matter.matterDescription : "",
-                        Email : value.email,
-                        Notification : value.notification,
-                    }
-                   */
+                        /*
+                        const tableData={
+                            id: value._id,
+                            Subject : value.title,
+                           
+                            TimeForReminder : value.timeForReminder,
+                            Matter : value.matter ?  value.matter.matterDescription : "",
+                            Email : value.email,
+                            Notification : value.notification,
+                        }
+                       */
+                    })
                 })
-            })
+            }
 
         },60*1000)
         const handleNavigation=(path, value)=>{
