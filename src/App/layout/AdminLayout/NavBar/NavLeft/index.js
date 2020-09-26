b61eb623f0f8f86fad4e817bca0f30eeeaf2c75b
Anas M.I <anas3rde@gmail.com>
2020-09-26 20:26:00 +0530

    removed dropdown from admin

--- src/App/layout/AdminLayout/NavBar/NavLeft/index.js
+++ src/App/layout/AdminLayout/NavBar/NavLeft/index.js
@@ -34,7 +34,7 @@ class NavLeft extends Component {
                 <ul className="navbar-nav mr-auto">
                     <li><a href={DEMO.BLANK_LINK} className="full-screen" onClick={this.props.onFullScreen}><i className={iconFullScreen.join(' ')} /></a></li>
                     <li className={navItemClass.join(' ')}>
-                        <Dropdown alignRight={dropdownRightAlign}>
+                        {/* <Dropdown alignRight={dropdownRightAlign}>
                             <Dropdown.Toggle variant={'link'} id="dropdown-basic">
                                 Dropdown
                             </Dropdown.Toggle>
@@ -45,7 +45,7 @@ class NavLeft extends Component {
                                     <li><a className="dropdown-item" href={DEMO.BLANK_LINK}>Something else here</a></li>
                                 </Dropdown.Menu>
                             </ul>
-                        </Dropdown>
+                        </Dropdown> */}
                     </li>
                     <li className="nav-item"><NavSearch/></li>
                 </ul>
