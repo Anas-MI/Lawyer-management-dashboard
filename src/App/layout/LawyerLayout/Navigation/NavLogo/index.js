8663143579d3c620dc0157ebb7b61bed621290fa
Anas M.I <anas3rde@gmail.com>
2020-10-09 18:41:35 +0530

    logo fixed

--- src/App/layout/LawyerLayout/Navigation/NavLogo/index.js
+++ src/App/layout/LawyerLayout/Navigation/NavLogo/index.js
@@ -9,13 +9,16 @@ const navLogo = (props) => {
     }
 
     return (
+        //Lawyer Dashboard
         <Aux>
             <div className="navbar-brand header-logo">
                  <a href={DEMO.BLANK_LINK} className="b-brand">
-                    <div className="b-bg">
-                        <i className="feather icon-trending-up" />
-                    </div>
-                    <span className="b-title">Case Management </span>
+                    {/* <div className="b-bg"> */}
+                        {/* <i className="feather icon-trending-up" /> */}
+                        <img style={{"width": "30%"}}src="https://res.cloudinary.com/casemanagement/image/upload/v1600287856/gtfwyjkqpb3auwrkftag.png" alt="Precedent Online"/>
+
+                    {/* </div> */}
+                    <span className="b-title">Precedent Online </span>
                  </a>
                 {/* <a href={DEMO.BLANK_LINK} className={toggleClass.join(' ')} id="mobile-collapse" onClick={props.onToggleNavigation}><span /></a> */}
             </div>
