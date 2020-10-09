90197913f8e085f64eb3203d62ffe5c05e2f5ba9
Anas M.I <anas3rde@gmail.com>
2020-10-09 18:41:18 +0530

    logo for dashboard fixed

--- src/App/layout/AdminLayout/Navigation/NavLogo/index.js
+++ src/App/layout/AdminLayout/Navigation/NavLogo/index.js
@@ -9,13 +9,17 @@ const navLogo = (props) => {
     }
 
     return (
+        //Admin dashbaord main
         <Aux>
             <div className="navbar-brand header-logo">
                  <a href={DEMO.BLANK_LINK} className="b-brand">
-                    <div className="b-bg">
-                        <i className="feather icon-trending-up" />
-                    </div>
-                    <span className="b-title">Case Management</span>
+          
+                        {/* <i className="feather icon-trending-up" /> */}
+                     
+                        <img style={{"width": "30%"}}src="https://res.cloudinary.com/casemanagement/image/upload/v1600287856/gtfwyjkqpb3auwrkftag.png" alt="Precedent Online"/>
+
+                    
+                    <span className="b-title">Precedent Online</span>
                  </a>
                 {/* <a href={DEMO.BLANK_LINK} className={toggleClass.join(' ')} id="mobile-collapse" onClick={props.onToggleNavigation}><span /></a> */}
             </div>
